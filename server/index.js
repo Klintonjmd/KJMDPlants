4eccbb13b6e4a5a2e6962c8dfc7ac3556d510be9
Klintonjmd <100642525+Klintonjmd@users.noreply.github.com>
2024-04-07 17:38:37 -0400

    Update to MySQL2 and connect to local database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const mysql = require('mysql2');
 
 app.use(cors())
+const db  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  port            : '3306',
+  user            : 'root',
+  password        : '21082',
+  database        : 'kjmdplants'
+});
 
 app.get('/', (req, res) => {
-  res.send('Text in the app.get in server/index.js');
+  db.query("INSERT INTO users (username, password) VALUES ('Testing', '123')", (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(result)
+    }
+  })
 })
 
 app.listen(8080, () => {
