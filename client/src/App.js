b2bba063c717813ab03716423f700df866519970
Klintonjmd <100642525+Klintonjmd@users.noreply.github.com>
2024-04-10 17:56:31 -0400

    Set up routing for sign up and sign in pages and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,9 @@
-import axios from 'axios';
-import { useState } from 'react'
-
 function App() {
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const submitHandler = e => {
-    e.preventDefault()
-    axios.post('http://localhost:8080/signup', {username: username, password: password})
-    .then((data)=> {
-      console.log(data)
-      setUsername('')//reset values to blank after post (submission)
-      setPassword('')
-    })
-  }
   return (
     <div>
-      <form className='mx-auto border-2 p-9 md:p-12 w-72 md:w-96 border-cyan-400 mt-36 h-84' onSubmit={submitHandler}>
-        <h3 className='pb=6 text-2xl text-center text-white'>Sign Up</h3>
-        <label className='block mb-1 text-xl text-cyan-400' htmlFor='username'>Username</label>
-        <input className='w-full h-8 p-1 mb-6 focus:outline-none' id='username' type='text' value={username} onChange={(e) => setUsername(e.target.value)} /> 
-        <label className='block mb-1 text-xl text-cyan-400' htmlFor='password'>Password</label>
-        <input className='w-full h-8 p-1 mb-6 focus:outline-none' id='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-        <div className='flex justify-between'>
-          <button className='px-3 py-1 rounded-sm bg-cyan-400' type='submit'>Submit</button>
-          <button className='px-3 py-1 rounded-sm bg-cyan-400' type='button'>Cancel</button>
-        </div>
-      </form>
+      <h1 className="text-2xl text-center text-white">Home Page</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements((
+    <Route path='/' element={<RootLayout />}>
+      <Route path='/' element={<App />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/signin' element={<Signin />} />
+    </Route>
+  )
+))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
