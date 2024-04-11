6d3ad05a1390d7a54acfc3b90bd47adadd00a436
Klintonjmd <100642525+Klintonjmd@users.noreply.github.com>
2024-04-11 19:01:54 -0400

    Add footer and anchor icons

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -10,9 +10,9 @@ export default function Navigation() {
       </div>
       <div className="flex-none">
         <ul className="px-1 menu menu-horizontal">
-        <li><Link className='text-white' to='/signup'>Sign Up</Link></li>
-        <li className='pl-16 text-white'><Link to='/signin'>Sign In</Link></li>
-        <li><a className='pl-16 text-white' href='http://klintonjmd.com/' target="_blank">Portfolio</a></li>
+        <li><Link className='' to='/signup'>Sign Up</Link></li>
+        <li className='pl-12'><Link to='/signin'>Sign In</Link></li>
+        <li><a className='pl-12' rel="noreferrer" href='http://klintonjmd.com/' target="_blank">My Portfolio</a></li>
         </ul>
       </div>
     </div>
